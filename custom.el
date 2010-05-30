cd0cd754d15f6e1531ea9477f41229376049b092
Alexey Romanov <alexey.v.romanov@gmail.com>
2010-05-30 12:51:37 +0400

    added custom.el

---
+++ custom.el
@@ -0,0 +1,30 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(LaTeX-XeTeX-command "xelatex -synctex=1")
+ '(TeX-engine (quote xetex))
+ '(TeX-view-program-list (quote (("Skim" "/Applications/Skim.app/Contents/SharedSupport/displayline %n %o %b"))))
+ '(TeX-view-program-selection (quote (((output-dvi style-pstricks) "dvips and gv") (output-dvi "xdvi") (output-pdf "Skim") (output-html "xdg-open"))))
+ '(appmenu-mode t)
+ '(cua-mode t nil (cua-base))
+ '(emacsw32-max-frames t)
+ '(emacsw32-style-frame-title t)
+ '(hfyview-quick-print-in-files-menu t)
+ '(menuacc-mode t)
+ '(noprint-hide-print-in-menus t)
+ '(noprint-hide-ps-print-in-menus t)
+ '(ourcomments-ido-ctrl-tab t)
+ '(rebind-keys-mode t)
+ '(recentf-mode t)
+ '(sex-mode t)
+ '(tabkey2-mode t)
+ '(w32-meta-style (quote w32-lr))
+ '(w32shell-shell (quote cmd)))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
