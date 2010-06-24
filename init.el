153214ccd90d8e059238b9a5ce7addb00a2aecd0
Alexey Romanov <alexey.v.romanov@gmail.com>
2010-06-24 19:48:00 +0400

    Started merge with new org-mode

--- init.el
+++ init.el
@@ -8,21 +8,14 @@
 ;; Load up Org Mode and Org Babel for elisp embedded in Org Mode files
 (setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
 
-(let* ((org-dir (expand-file-name
-		 "lisp" (expand-file-name
-			 "org" (expand-file-name
-				"src" dotfiles-dir))))
-       (org-contrib-dir (expand-file-name
+(add-to-list 'load-path (expand-file-name
                          "lisp" (expand-file-name
-                                 "contrib" (expand-file-name
-                                            ".." org-dir))))
-       (load-path (append (list org-dir org-contrib-dir)
-			  (or load-path nil))))
-  ;; load up Org-mode and Org-babel
-  (setq org-replace-disputed-keys t)
-  (setq org-return-follows-link t)
-  (require 'org-install)
-  (require 'org-babel-init))
+                                 "org" (expand-file-name
+                                        "src" dotfiles-dir))))
+;; Load up Org Mode and Babel
+(setq org-replace-disputed-keys t)
+(setq org-return-follows-link t)
+(require 'org-install)
 
 ;; load up the main file
 (org-babel-load-file (expand-file-name "starter-kit.org" dotfiles-dir))
