ac68b2b22610a579db6f8a5d4047625fba806ef1
Alexey Romanov <alexey.v.romanov@gmail.com>
2010-06-25 23:59:14 +0400

    Removed the use of Win key as Meta and second initialization of CUA-mode.

--- custom.el
+++ custom.el
@@ -4,7 +4,6 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(appmenu-mode t)
- '(cua-mode t nil (cua-base))
  '(emacsw32-max-frames t)
  '(emacsw32-style-frame-title t)
  '(hfyview-quick-print-in-files-menu t)
@@ -16,7 +15,7 @@
  '(recentf-mode t)
  '(sex-mode t)
  '(tabkey2-mode t)
- '(w32-meta-style (quote w32-lr))
+ '(w32shell-cygwin-bin "C:\\cygwin\\bin")
  '(w32shell-shell (quote cmd)))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
